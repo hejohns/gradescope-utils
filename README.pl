13064a9c9c1da2e48215f28d7d276d13d168265d
hejohns <hejohns@umich.edu>
2022-09-25 16:54:32 -0400

    update README.pl

--- README.pl
+++ README.pl
@@ -23,21 +23,19 @@ Each file should contain its own documentation
 
 Below is just a overview
 
-=head2 ./upload
-
-scripts to upload to gradescope
-
 originally a port of
-https://github.com/eecs490/Assignment-8-Gradescope
+L<https://github.com/eecs490/Assignment-8-Gradescope>
 during W22
 
-=head3 Translate.pm
+=head2 bin
 
-Handles token/uid ↦ uniqname translation
+=head3 download.pl
 
-A carryover from the scripts' origins w/ A8
+Contrary to the name, doesn't actually download anything
 
-Just make uid = uniqname if you don't actually need uid handling
+Takes a zip of submissions from Gradescope and formats the data so gen_submissions.pl can work on it
+
+(for non-A8 workflows)
 
 =head3 gen_submissions.pl
 
@@ -47,7 +45,21 @@ The single csv is from A8-- a sqlite dump
 
 =head3 upload.pl
 
-uploads all submissions output by ./gen_submissions.pl
-(or in the correct format, since ./gen_submissions.pl is fairly A8 specific)
+uploads all submissions output by gen_submissions.pl
+(or in the correct format, since gen_submissions.pl is fairly A8 specific)
+
+=head2 lib
+
+=head3 Translate.pm
+
+Handles token/uid ↦ uniqname translation
+
+A carryover from the scripts' origins w/ A8
+
+Just make uid = uniqname if you don't actually need uid handling
+
+=head3 CurlGradescope.pm
+
+Common curl stuff between scripts
 
 =cut
