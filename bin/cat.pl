eba12d72c5edb1c38cb6f524888c3da6bbabb1e9
hejohns <hejohns@umich.edu>
2022-12-28 12:12:58 -0500

    helper lambdas

---
+++ bin/cat.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+use v5.36;
+use utf8;
+use strictures 2; # nice `use strict`, `use warnings` defaults
+use open qw(:utf8); # try to use Perl's internal Unicode encoding for everything
+BEGIN{$diagnostics::PRETTY = 1} # a bit noisy, but somewhat informative
+use diagnostics -verbose;
+
+# Carp
+    use Carp;
+    use Carp::Assert;
+# filepath functions
+    use Cwd qw(abs_path);
+    use File::Basename qw(basename dirname);
+    use File::Spec;
+# misc file utilities
+    use File::Temp;
+    use File::Slurp;
+    use Text::CSV;
+    use JSON;
+    use YAML::XS;
+# misc scripting IO utilities
+    # `capture_stdout` for backticks w/o shell
+    use Capture::Tiny qw(:all);
+# option/arg handling
+    use Getopt::Long qw(:config gnu_getopt auto_version); # auto_help not the greatest
+    use Pod::Usage;
+# use local modules
+    use lib (
+        dirname(abs_path($0)),
+        ); # https://stackoverflow.com/a/46550384
+ 
+# turn on features
+    use feature 'try';
+    no warnings 'experimental::try';
+
+    our $VERSION = version->declare('v2022.11.13');
+# end prelude
+
+my $submission_dir = $ARGV[0];
+system('cat', glob "$submission_dir/*");
